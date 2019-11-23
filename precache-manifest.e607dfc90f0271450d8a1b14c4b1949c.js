4940eeaa0d1c2b24dd1289a57c1ba0b6b0be99e2
jackhwolf <jwolf22@wisc.edu>
2019-11-23 12:03:20 -0600

    Updates

---
+++ precache-manifest.e607dfc90f0271450d8a1b14c4b1949c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf437f2a5c51ac5e1601a69b67dac8f",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "2485cc626f84dd3831a0",
+    "url": "/blog/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7f5f3c3b88fdad9d34ad",
+    "url": "/blog/static/js/2.34b5a208.chunk.js"
+  },
+  {
+    "revision": "2485cc626f84dd3831a0",
+    "url": "/blog/static/js/main.9387b00a.chunk.js"
+  },
+  {
+    "revision": "3ab9dfc9000e704fe6b2",
+    "url": "/blog/static/js/runtime-main.6448740b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blog/static/media/logo.25bf045c.svg"
+  }
+]);
