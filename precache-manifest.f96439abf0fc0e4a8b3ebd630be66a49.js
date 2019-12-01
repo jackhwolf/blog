4670dd7421013ec39e2b5aa3f691770f99b48299
jackhwolf <jwolf22@wisc.edu>
2019-11-30 19:28:12 -0800

    Updates

--- precache-manifest.f96439abf0fc0e4a8b3ebd630be66a49.js
+++ precache-manifest.f96439abf0fc0e4a8b3ebd630be66a49.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cc134bbf00be459ba751adcd102c8838",
+    "revision": "f9b799ccdb7221a218303937e4e44164",
     "url": "/blog/index.html"
   },
   {
-    "revision": "fd8ddc4fc7987b3c7554",
+    "revision": "6ad9b2086ef4a1e6ee8c",
     "url": "/blog/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "b59787f4700355483eeb",
-    "url": "/blog/static/css/main.5f49ed10.chunk.css"
+    "revision": "6a1821fafd0870612fcf",
+    "url": "/blog/static/css/main.145b6ff0.chunk.css"
   },
   {
-    "revision": "fd8ddc4fc7987b3c7554",
-    "url": "/blog/static/js/2.725cc505.chunk.js"
+    "revision": "6ad9b2086ef4a1e6ee8c",
+    "url": "/blog/static/js/2.762bc8f1.chunk.js"
   },
   {
-    "revision": "b59787f4700355483eeb",
-    "url": "/blog/static/js/main.2c56f2a2.chunk.js"
+    "revision": "6a1821fafd0870612fcf",
+    "url": "/blog/static/js/main.d1ca57c3.chunk.js"
   },
   {
     "revision": "3ab9dfc9000e704fe6b2",
@@ -34,6 +34,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "25bf045ca257e971124f3997d89f321c",
     "url": "/blog/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5d96e7bfd04ccc45ade8813e12c3b8e8",
+    "url": "/blog/static/media/post.5d96e7bf.md"
   },
   {
     "revision": "82a318ccea22a8af9fc4590f41df9238",
