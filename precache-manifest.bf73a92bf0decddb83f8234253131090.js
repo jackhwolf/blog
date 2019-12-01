39f513b11e9368bc8d852da464dc9b7d6afdb68a
jackhwolf <jwolf22@wisc.edu>
2019-12-01 12:38:24 -0800

    Updates

---
+++ precache-manifest.bf73a92bf0decddb83f8234253131090.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65f5e8f2fed47945ac010a434c395149",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "b58c52217c9978420e33",
+    "url": "/blog/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1ee5ac0d7a24c214d5b1",
+    "url": "/blog/static/css/main.579aee9f.chunk.css"
+  },
+  {
+    "revision": "b58c52217c9978420e33",
+    "url": "/blog/static/js/2.d70095e0.chunk.js"
+  },
+  {
+    "revision": "1ee5ac0d7a24c214d5b1",
+    "url": "/blog/static/js/main.82dde35c.chunk.js"
+  },
+  {
+    "revision": "3ab9dfc9000e704fe6b2",
+    "url": "/blog/static/js/runtime-main.6448740b.js"
+  },
+  {
+    "revision": "a9b726c4ea03dc3e07d6818fadf2b7a1",
+    "url": "/blog/static/media/awslogo.a9b726c4.png"
+  },
+  {
+    "revision": "bd0b2dbd839f638fefbcbf591d8c9ff3",
+    "url": "/blog/static/media/bucky.bd0b2dbd.png"
+  },
+  {
+    "revision": "ea92008be92e35cc7b6d33e12110dcee",
+    "url": "/blog/static/media/flasklogo.ea92008b.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/blog/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "69aba11300c66c986904125cb012f25e",
+    "url": "/blog/static/media/post.69aba113.md"
+  },
+  {
+    "revision": "82a318ccea22a8af9fc4590f41df9238",
+    "url": "/blog/static/media/reactjslogo.82a318cc.png"
+  }
+]);
